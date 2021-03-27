d3f92ef62a027f0c7679fb948b32ae51ddf16a05
Sankalpa Dutta <41365024+SankalpaDutta@users.noreply.github.com>
2021-03-28 00:32:08 +0100

    Add files via upload

--- NLPbasics.py
+++ NLPbasics.py
@@ -2,3 +2,23 @@ import spacy
 import numpy as np
 
 nlp = spacy.load('en_core_web_sm')
+
+doc=nlp(u' Tesla is buying U.A.E. for $6 million')
+
+for token in doc:
+    print(token.text,token.pos,token.pos_)
+
+
+doc2 = nlp(u'This is first sentence. This is second sentence. This is third sentence')
+
+
+for sentence in doc2.sents:
+    print(sentence)
+
+print(doc2[5].is_sent_start)  #returns if the word is starting the sentence "This"
+
+
+
+ #Tokenization
+
+ 
